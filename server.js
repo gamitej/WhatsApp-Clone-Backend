8d74904a06b836ff17ca6fce58af3fd5e6964070
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-23 11:13:05 +0530

    client url added in .env

--- server.js
+++ server.js
@@ -17,13 +17,14 @@ const { SocketConnection } = require("./socket.js");
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
+const CLIENT_URL = process.env.CLIENT_URL;
 
 // CONFIGURATION
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
   },
 });
 
